049560c97a5172eb8c76d3fd7025aaa979331b4a
Hélio Nogueira Cardoso <rinder_omna@outlook.com>
2022-06-16 20:54:06 -0300

    Modularizando compare

--- zip_to_send/funcionalidades.h
+++ zip_to_send/funcionalidades.h
@@ -6,6 +6,9 @@ void funcionalidade1(int tipo_do_arquivo, string_t csv, string_t binario_saida);
 // Imprime todos os registros não removidos em sequência de um arquivo binário de tipo passado
 void funcionalidade2(int tipo_do_arquivo, string_t binario_entrada);
 
+// Checa que as informações do registro batem com as especificaçẽos em nome_campos e valor_campos
+int registro_encontrado(registro_t *reg, string_t *nome_campos, string_t *valor_campos, int n);
+
 // Imprime todos os registro não removidos de um arquivo binário de tipo passado, desde que o
 // registro satisfaça n condições para valores campo, condições estas lidas nas próximas n
 // linhas da entrada padrão
