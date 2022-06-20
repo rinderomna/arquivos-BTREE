5a67e935354c1982585dcd66a18e3500fc686d28
Hélio Nogueira Cardoso <rinder_omna@outlook.com>
2022-06-20 11:25:00 -0300

    func6 tirando campo de teste da main

--- main.c
+++ main.c
@@ -40,31 +40,6 @@ int main() {
     }
 
     switch (codigo_de_funcionalidade) {
-        //-- Campo de Teste
-        case -1: // -1 tipo2 indiceTeste.bin <algum_id_para_teste>
-            binario_entrada = campos_de_comando[2];
-            long long int id_buscado = atoi(campos_de_comando[3]);
-
-            indice = ler_indice(tipo, binario_entrada);
-
-            long long int retorno = busca_no_indice(indice, tipo, id_buscado, NULL);
-
-            printf("%lld\n", retorno);
-
-            destruir_indice(indice);
-
-            break;
-        case -2: // -2 tipo2 indiceTeste.bin
-            binario_entrada = campos_de_comando[2];
-
-            indice = ler_indice(tipo, binario_entrada);
-
-            imprime_indice(indice, tipo);
-
-            destruir_indice(indice);
-
-            break;
-        //--
         case 1:
             csv = campos_de_comando[2];
             binario_saida = campos_de_comando[3];
